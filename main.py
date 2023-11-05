f35291d4409136c0a6f2516b29235c3453659709
ascot20 <danielotchere18@gmail.com>
2023-11-05 15:08:03 +0000

    handled errors

--- main.py
+++ main.py
@@ -6,20 +6,27 @@ import random
 BACKGROUND_COLOR = "#B1DDC6"
 delay = None
 random_word = None
+translations = []
+
 
 # import csv file
-try:
-    df = pandas.read_csv('./data/words_to_learn.csv')
-except FileNotFoundError:
-    df = pandas.read_csv('./data/french_words.csv')
-    translations = df.to_dict('records')
-else:
-    translations = df.to_dict('records')
+def import_csv():
+    global translations
+    try:
+        df = pandas.read_csv('./data/words_to_learn.csv')
+    except FileNotFoundError:
+        df = pandas.read_csv('./data/french_words.csv')
+        translations = df.to_dict('records')
+    except pandas.errors.EmptyDataError:
+        df = pandas.read_csv('./data/french_words.csv')
+        translations = df.to_dict('records')
+    else:
+        translations = df.to_dict('records')
 
 
 # ------------------------Next word mechanism------------------------#
 def change(button_clicked):
-    global delay, random_word
+    global delay, random_word, translations
     if delay is not None:
         window.after_cancel(delay)
     if button_clicked == 'correct':
@@ -27,7 +34,11 @@ def change(button_clicked):
         new_df = pandas.DataFrame(translations)
         new_df.to_csv('./data/words_to_learn.csv', index=False)
 
-    random_word = random.choice(translations)
+    try:
+        random_word = random.choice(translations)
+    except IndexError:
+        import_csv()
+        random_word = random.choice(translations)
     card_canvas.itemconfig(card_image, image=front_card)
     card_canvas.itemconfig(title, text='French', fill='black')
     card_canvas.itemconfig(word, text=random_word['French'], fill='black')
@@ -66,6 +77,7 @@ wrong_image = tkinter.PhotoImage(file='./images/new_wrong.png')
 wrong_button = tkinter.Button(image=wrong_image, command=lambda: change('wrong'))
 wrong_button.grid(column=0, row=1)
 
+import_csv()
 change("None")
 
 window.mainloop()
