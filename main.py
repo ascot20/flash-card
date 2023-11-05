681c2300edf33c24c24966cc3c050d4c6b9c5e96
ascot20 <danielotchere18@gmail.com>
2023-11-05 10:44:41 +0000

    added word change functionality

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 import tkinter
+import pandas
+import random
 
 # constants
 BACKGROUND_COLOR = "#B1DDC6"
+
+# import csv file
+df = pandas.read_csv('./data/french_words.csv')
+translations = df.to_dict('records')
+
+
+# ------------------------Next word mechanism------------------------#
+def change():
+    random_word = random.choice(translations)
+    card_canvas.itemconfig(title, text='French')
+    card_canvas.itemconfig(word, text=random_word['French'])
+
 
 # ------------------------UI------------------------#
 window = tkinter.Tk()
@@ -20,11 +34,11 @@ card_canvas.grid(column=0, row=0, columnspan=2)
 
 # buttons
 correct_image = tkinter.PhotoImage(file='./images/new_right.png')
-correct_button = tkinter.Button(image=correct_image)
+correct_button = tkinter.Button(image=correct_image, command=change)
 correct_button.grid(column=1, row=1)
 
 wrong_image = tkinter.PhotoImage(file='./images/new_wrong.png')
-wrong_button = tkinter.Button(image=wrong_image)
+wrong_button = tkinter.Button(image=wrong_image, command=change)
 wrong_button.grid(column=0, row=1)
 
 window.mainloop()
